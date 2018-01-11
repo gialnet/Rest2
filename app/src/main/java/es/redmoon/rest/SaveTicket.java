98e071779777b1e02ca0228a0f506ff8debcb143
Antonio Perez Caballero <antonio.gialnet@gmail.com>
2018-01-11 19:07:02 +0000

    new class for version support

---
+++ app/src/main/java/es/redmoon/rest/SaveTicket.java
@@ -0,0 +1,16 @@
+package es.redmoon.rest;
+
+/**
+ * Created by antonio on 11/01/2018.
+ */
+
+public class SaveTicket {
+    private final String ver= "1.0";
+
+    public SaveTicket() {
+    }
+
+    public String getVer() {
+        return ver;
+    }
+}
